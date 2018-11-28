3e467d93a58ecebb7184ae76d11d2298e9faca50
jumproper <dylanmplummer@gmail.com>
2018-11-28 00:58:59 -0500

    Add randomized group number to graphGen

--- GraphGen.py
+++ GraphGen.py
@@ -1,23 +1,21 @@
 import json
 import numpy as np
+import random
 import os
 
-def GenerateGraph(numVerts, density, maxWeight):
+def GenerateGraph(numVerts, density, maxWeight, numGroups=5):
     nodes = [] ; links = []
     #generate nodes
     for x in range(numVerts):
-        nodes.append({'group': 1, 'name': x})
+        nodes.append({'group': random.randrange(1, numGroups), 'name': x})
     #generate links
     for x in nodes:
         for y in nodes[x['name']+1:]:
             if np.random.binomial(1, density) == 1:
                 w = np.random.randint(maxWeight)
-                links.append({'source': x['name'], 'target': y['name'], 'value': w})      
-    return {'links': links,'nodes': nodes} 
+                links.append({'source': x['name'], 'target': y['name'], 'value': w})
+    return {'links': links,'nodes': nodes}
 
 def GraphToJSON(data):
     os.remove('static/graph.json')
     with open('static/graph.json','w') as f: json.dump(data,f)
-	
-	
-GraphToJSON(GenerateGraph(50, 0.05, 10))
