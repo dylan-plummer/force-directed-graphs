f58deb5856db8dc0177a5fb19744634cd7b891bd
jumproper <dylanmplummer@gmail.com>
2018-11-28 00:59:58 -0500

    Align input fields

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 import configparser
 from flask import Flask, render_template, request
 from GraphGen import GenerateGraph, GraphToJSON
-from nocache import nocache
 import mysql.connector
 
 # Read configuration from file.
@@ -34,17 +33,18 @@ def sql_execute(sql):
 
 
 @app.route('/', methods=['GET', 'POST'])
-@nocache
 def template_response_with_data():
     print(request.form)
     template_data = {}
-    num_verts = request.form['num-verts']
     try:
-	    GraphToJSON(GenerateGraph(int(num_verts), 0.05, 10))
+        num_verts = request.form['num-verts']
+        GraphToJSON(GenerateGraph(int(num_verts), 0.05, 10))
     except TypeError as e1:
 	    print('Number of vertices must be an integer')
     except ValueError as e2:
 	    print('Number of vertices must not be empty')
+    except KeyError as e3:
+        print('Could not find the resource you selected')
     return render_template('index.html', template_data=template_data)
 
 @app.context_processor
