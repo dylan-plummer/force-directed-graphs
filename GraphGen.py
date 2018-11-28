4cb35a372bc37dd15771493e966e755e93ff8f56
tuccim98 <mrt62@case.edu>
2018-11-28 14:03:36 -0500

    Trees r good

--- GraphGen.py
+++ GraphGen.py
@@ -10,60 +10,46 @@ import os
 def GenerateGraph(numVerts, density, maxWeight, style, numGroups=5):
     nodes = [] ; links = []
 
-    #generate nodes
+    # Generate Nodes
     for x in range(numVerts):
         nodes.append({'group': np.random.randint(numGroups), 'name': x})
 
-    #generate links
-    if style == "k":
+    if style == "k":  # Generate Complete
+        print("Generating Complete...")
         for x in nodes:
             for y in nodes[x['name']+1:]:
                 if np.random.binomial(1, density) == 1:
                     w = np.random.randint(maxWeight)
                     links.append({'source': x['name'], 'target': y['name'], 'value': w})
 
-    if style == "t": #Generate Tree
-        print("t")
-        N = nodes                       # Temp node array
-        C = int(len(N)*density*.33)     # Random MaxChildren
-        print("C: "+str(C))
-        r = N[0]                        # Set root node
-        N = N[1:] ; N.reverse()         # Remove root node from N, reverse N
-        print("---------")
+    elif style == "t":  # Generate Tree
+        print("Generating Tree...")
+        N = nodes
+        C = int(len(N)*density*.33)
+        if C == 0: C == 1
+        r = N[0]
+        N = N[1:] ; N.reverse()
         while N != []:
-            #print("<N "+str(len(N))+">" + str(N))
-            c = np.random.randint(C)+1  # Choose num children
-            #print("c: "+str(c))
-
+            c = np.random.randint(C)+1
             if len(N) <= c:
                 for n in N:
-                    #print(">>end")
                     w = np.random.randint(maxWeight)
                     links.append({'source': n['name'], 'target': r['name'], 'value': w})
-                #print(N)
-                #print(">>"+str(len(links)))
                 N=[]
-
             else:
-                children = N[:c]    # Get children
-                N = N[c:]           # Remove children from N
-                #print("<Childs "+str(len(children))+">"+str(N[:c]))
-                #print("<N "+str(len(N))+">")
-                #choose parent, leave parent in N
-                pIndex = np.random.randint(len(N))      # Random parent node index
-                P = N[pIndex]
-                #print("<P>"+str(P))
+                children = N[:c]; N = N[c:]
+                P = N[np.random.randint(len(N))]
                 for child in children:
                     w = np.random.randint(maxWeight)
                     links.append({'source':child['name'], 'target': P['name'], 'value': w})
-            print("L"+str(links))
+
+    elif style == "c": # Generate Connected Components
+        print("Generating Connected Components...")
+
+
+
 
     return {'links': links,'nodes': nodes}
-
-#GenerateGraph(20,.5,10,"t")
-
-
-
 
 
 def GraphToJSON(data):
