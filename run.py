97e2f2d5c49f732b0d806934df0714692be70e6c
jumproper <dylanmplummer@gmail.com>
2018-11-27 22:45:53 -0500

    Add random graph button

--- run.py
+++ run.py
@@ -32,12 +32,12 @@ def sql_execute(sql):
 @app.route('/', methods=['GET', 'POST'])
 def template_response_with_data():
     print(request.form)
-    if "buy-book" in request.form:
-        book_id = int(request.form["buy-book"])
-        sql = "delete from book where id={book_id}".format(book_id=book_id)
-        sql_execute(sql)
+    if "random-graph" in request.form:
+        book_id = int(request.form["random-graph"])
+        #sql = "delete from book where id={book_id}".format(book_id=book_id)
+        #sql_execute(sql)
     template_data = {}
-    sql = "select id, title from book order by title"
+    #sql = "select id, title from book order by title"
     books = sql_query(sql)
     template_data['books'] = books
     return render_template('index.html', template_data=template_data)
